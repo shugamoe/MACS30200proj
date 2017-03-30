eefd1c0a6a36ebcc1663ffb98842554f074fedba
Julian McClellan <juliancmcclellan@gmail.com>
2017-03-29 22:43:44 -0500

    Hopefuly should have the basic stats gathering for Author submissions done by now.

--- changemyview/cmv_scrape_mod.py
+++ changemyview/cmv_scrape_mod.py
@@ -18,7 +18,7 @@ START_BDAY_2016 = 1461110400
 END_BDAY_2016 = 1461196799
 
 
-class CmvScraperModder:
+class CMVScraperModder:
     '''
     Class to scrape /r/changemyview for MACS 302 and possibly thesis.
     '''
@@ -45,17 +45,16 @@ class CmvScraperModder:
     @classmethod
     def from_pickle(cls, pickle_path):
         '''
-        Recovers an instance of CmvScraperModder from pickle file.
+        Recovers an instance of CMVScraperModder from pickle file.
         '''
         pass 
         
-
     def get_submissions(self):
         '''
         This function gathers the submission IDs for submissions in 
         /r/changemyview
         '''
-        init_col_names = ['id', 'author', 'praw_inst']
+        init_col_names = ['id', 'author', 'sub_inst']
         sub_df_dict = {col_name: [] for col_name in init_col_names}
 
         subs_gathered = 0
@@ -64,34 +63,34 @@ class CmvScraperModder:
             subs_gathered += 1
             sub_df_dict['author'].append(str(sub.author))
             sub_df_dict['id'].append(sub.id)
-            sub_df_dict['praw_inst'].append(sub)
+            sub_df_dict['sub_inst'].append(sub)
 
         print('{} submissions gathered'.format(subs_gathered))
         df = pd.DataFrame(sub_df_dict)
         self.cmv_subs = df.set_index('id', drop = False)
 
-    def update_subs_df(self):
+    def update_cmv_submissions(self):
         '''
         This function retrieves following information about submissions:
             - Whether the OP awarded a delta
             - How many deltas the OP awarded
             - Number of top level replies
         '''
-        if hasattr(self, 'subs_df'):
+        if hasattr(self, 'cmv_subs'):
             pass
         else:
             self.get_submissions()
 
         all_subs = self.cmv_subs
-        valid_subs = all_subs[all_subs['author'] != '[deleted]'][['praw_inst']]
+        valid_subs = all_subs[all_subs['author'] != '[deleted]'][['sub_inst']]
 
-        lambda_sub_info = lambda sub_inst: self._get_sub_info(sub_inst)
-        valid_subs[list(CmvSubmission.VARS_TEMPLATE.keys())] = (
-                    valid_subs['praw_inst'].apply(lambda_sub_info))
+        valid_subs[list(CMVSubmission.STATS_TEMPLATE.keys())] = (
+                valid_subs['sub_inst'].apply(lambda sub_inst:
+                    CMVSubmission(sub_inst).get_stats_series()))
 
         # TODO(jcm): Get index matching without column duplication working, 
-        # matching objects is slower than matching stringsmmi
-        self.cmv_subs = all_subs.merge(valid_subs, on = 'praw_inst')
+        # matching objects is slower than matching strings
+        self.cmv_subs = all_subs.merge(valid_subs, on='sub_inst')
 
     def _get_sub_info(self, sub_inst):
         '''
@@ -101,10 +100,10 @@ class CmvScraperModder:
             - How many deltas the OP awarded
             - Number of top level replies
         '''
-        Submission = CmvSubmission(sub_inst, self.praw_agent)
+        Submission = CMVSubmission(sub_inst)
         Submission.parse_root_comments()
 
-        return(Submission.get_series())
+        return(Submission.get_stats_series())
     
     def get_author_histories(self):
         '''
@@ -115,13 +114,13 @@ class CmvScraperModder:
             self.get_submissions()
         
         get_auth_hist_vrized = np.vectorize(self._get_author_history,
-                otypes = '?')
+                otypes='?') # otypes kwarg to avoid double appplying func
         get_auth_hist_vrized(self.cmv_subs['author'].unique())
 
     def _get_author_history(self, author):
         '''
         '''
-        SubAuthor = CmvSubAuthor(self.praw_agent.redditor(author))
+        SubAuthor = CMVSubAuthor(self.praw_agent.redditor(author))
         try:
             SubAuthor.get_history_for('comments')
             SubAuthor.get_history_for('submissions')
@@ -140,7 +139,24 @@ class CmvScraperModder:
                     SubAuthor.get_post_df('submissions'))
         else:
             self.cmv_author_subs = SubAuthor.get_post_df('submissions')
-    
+
+    def update_author_submissions(self):
+        '''
+        '''
+        if hasattr(self, 'cmv_author_subs'):
+            pass
+        else:
+            self.get_author_histories()
+        
+        sub_inst_series = self.cmv_author_subs[['sub_inst']]
+
+        sub_inst_series[list(CMVAuthSubmission.STATS_TEMPLATE.keys())] = (
+                sub_inst_series['sub_inst'].apply(
+                    lambda sub_inst: CMVAuthSubmission(sub_inst).get_stats_series()
+                ))
+        self.cmv_author_subs = self.cmv_author_subs.merge(sub_inst_series, on=
+                'sub_inst')
+
     @staticmethod
     def make_output_dir(dir_name):
         '''
@@ -158,22 +174,23 @@ class CmvScraperModder:
 # Would like to have this inherit from praw's submissions class but with the way
 # I'm scraping the data I would have to tinker with a praw's sublisting class 
 # and subreddit class.
-class CmvSubmission:
+class CMVSubmission:
     '''
     A class of a /r/changemyview submission
     '''
-    VARS_TEMPLATE = {'num_root_comments': 0,
+    STATS_TEMPLATE = {'num_root_comments': 0,
                      'num_user_comments': 0,
                      'OP_gave_delta': False,
                      'num_deltas_from_OP': 0}
 
-    def __init__(self, sub_inst, praw_agent):
-        self.praw_agent = praw_agent
+    def __init__(self, sub_inst):
         self.submission = sub_inst
         self.author = str(self.submission.author)
 
         # Important Variables to track
-        self.vars = self.VARS_TEMPLATE
+        self.stats = self.STATS_TEMPLATE
+
+        self.parsed = False 
 
     def parse_root_comments(self, comment_tree=None):
         '''
@@ -187,9 +204,11 @@ class CmvSubmission:
             elif com.stickied:
                 continue # Sticked comments are not replies to view
             else:
-                self.vars['num_user_comments'] += 1
-                self.vars['num_root_comments'] += 1
+                self.stats['num_user_comments'] += 1
+                self.stats['num_root_comments'] += 1
                 self.parse_replies(com.replies)
+
+        self.parsed = True
 
     def parse_replies(self, reply_tree):
         '''
@@ -200,7 +219,7 @@ class CmvSubmission:
             if str(reply.author) == 'DeltaBot':
                 self.parse_delta_bot_comment(reply)
             else:
-                self.vars['num_user_comments'] += 1
+                self.stats['num_user_comments'] += 1
 
     def parse_delta_bot_comment(self, comment):
         '''
@@ -214,24 +233,26 @@ class CmvSubmission:
             # (Submission are always by OP, comments are not.)
             if isinstance(parent_com, praw.models.Comment):
                 if str(parent_com.author) == self.author:
-                    self.vars['OP_gave_delta'] = True
-                    self.vars['num_deltas_from_OP'] += 1
+                    self.stats['OP_gave_delta'] = True
+                    self.stats['num_deltas_from_OP'] += 1
 
-    def get_series(self):
+    def get_stats_series(self):
         '''
         This function returns a series so this class can update the submissions
         dataframe
         '''
-        return(pd.Series(self.vars))
+        if not self.parsed:
+            self.parse_root_comments()
+        return(pd.Series(self.stats))
 
 
 # TODO(jcm): Implement the inheritance from praw's Redditor class, would be a 
 # more effective use of OOP
-class CmvSubAuthor:
+class CMVSubAuthor:
     '''
     Class for scraping the history of an author of /r/changemyview
     '''
-    VARS_TEMPLATE = {'sub_id': [],
+    STATS_TEMPLATE = {'sub_id': [],
                      'com_id': [],
                      'sub_inst': [],
                      'com_inst': []}
@@ -243,7 +264,7 @@ class CmvSubAuthor:
         self.user_name = str(redditor_inst.name)
 
         # Important variables to track
-        self.history = self.VARS_TEMPLATE
+        self.history = self.STATS_TEMPLATE
 
     def get_history_for(self, post_type):
         '''
@@ -299,7 +320,7 @@ class CmvSubAuthor:
     def get_post_df(self, post_type):
         '''
         This function returns a series so this class can update the authors'
-        comments or submissions dataframe in CmvScraperModder.
+        comments or submissions dataframe in CMVScraperModder.
         '''
         attribution_dict = {post_type_key: value for post_type_key, value in
                 self.history.items() if post_type[:3] == 
@@ -307,8 +328,31 @@ class CmvSubAuthor:
         attribution_dict.update({'author': self.user_name})
         return(pd.DataFrame(attribution_dict))
 
+# TODO(jcm): Make CMVSubmission inherit from CMVAuthSubmission
+class CMVAuthSubmission:
+    '''
+    '''
+    STATS_TEMPLATE = {'created_utc': None,
+                     'score': None,
+                     'subreddit': None}
+    def __init__(self, submission_inst):
+        '''
+        '''
+        self.submission = submission_inst
+        self.stats = self.STATS_TEMPLATE
+        self.parsed = False
+
+        # Stats that can be gathered right off the bat
+        self.stats['created_utc'] = self.submission.created_utc
+        self.stats['score'] = self.submission.score
+        self.stats['subreddit'] = self.submission.subreddit_name_prefixed
+
+    def get_stats_series(self):
+        '''
+        '''
+        return(pd.Series(self.stats))
 
 if __name__ == '__main__':
-    SModder = CmvScraperModder(START_BDAY_2016, END_BDAY_2016)
-    SModder.update_subs_df()
-    SModder.get_author_histories()
+    SModder = CMVScraperModder(START_BDAY_2016, END_BDAY_2016)
+    SModder.update_cmv_submissions()
+    SModder.update_author_submissions()
