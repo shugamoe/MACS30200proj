51a24b3c8a5640df0100f4bdf3e1c48228cf93c4
Julian McClellan <juliancmcclellan@gmail.com>
2017-05-25 11:29:32 -0500

    Filtering out mod messages now

--- changemyview/process_pkl.py
+++ changemyview/process_pkl.py
@@ -11,6 +11,9 @@ SID = SentimentIntensityAnalyzer()
 LINK_RE = re.compile(r"http\S*")
 
 
+MOD_MES_RE = re.compile("\n_____\n\n.*")
+re.sub(exp, '', top_submission.selftext)
+
 def main():
     """
     Does transformations on cmv_auth_subs.pkl and cmv_subs.pkl
@@ -20,6 +23,8 @@ def main():
 
     cmv_auth_subs = cmv_auth_subs[["sub_id", "title", "author", "created_utc", "content",
                                    "score", "subreddit"]]
+    cmv_auth_subs["content"] = cmv_auth_subs.content.apply(
+            lambda text: re.sub(MOD_MES_RE, "", text))
     cmv_auth_subs["deleted"] = cmv_auth_subs.content.str.contains(r"^\[deleted\]$")
     cmv_auth_subs["removed"] = cmv_auth_subs.content.str.contains(r"^\[removed\]$")
     cmv_auth_subs["empty"] = cmv_auth_subs.content.str.contains("^$")
@@ -33,6 +38,8 @@ def main():
     cmv_subs = cmv_subs.apply(pd.to_numeric, errors="ignore")
     cmv_auth_subs = cmv_auth_subs.apply(pd.to_numeric, errors="ignore")
     cmv_subs = cmv_subs[cmv_subs.title.str.contains("\[Podcast\]") == False]
+    cmv_subs["content"] = cmv_subs.content.apply(
+            lambda text: re.sub(MOD_MES_RE, "", text))
     cmv_subs["sentiment"] = cmv_subs.content.apply(lambda text:
                                                    SID.polarity_scores(text)["compound"])
 
